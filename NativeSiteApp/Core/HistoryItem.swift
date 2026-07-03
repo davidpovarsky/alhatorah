da3367d56ff21e798d45a37bbf3b81eaf889b2c6
davidpovarsky <davidpovarski1@gmail.com>
2026-07-03 13:28:02 +0300

    Add site-aware history metadata

--- NativeSiteApp/Core/HistoryItem.swift
+++ NativeSiteApp/Core/HistoryItem.swift
@@ -5,12 +5,14 @@ struct HistoryItem: Codable, Identifiable, Equatable {
     let title: String
     let urlString: String
     let visitedAt: Date
+    let siteID: String?
 
-    init(id: UUID = UUID(), title: String, urlString: String, visitedAt: Date = Date()) {
+    init(id: UUID = UUID(), title: String, urlString: String, visitedAt: Date = Date(), siteID: String? = nil) {
         self.id = id
         self.title = title
         self.urlString = urlString
         self.visitedAt = visitedAt
+        self.siteID = siteID
     }
 
     var url: URL? { URL(string: urlString) }
